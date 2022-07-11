b62ec2a8208f3394ac8c6337125dc3168ad3ff9c
Eduardo Gobbato <102391940+Egobbato@users.noreply.github.com>
2022-07-10 23:52:13 -0300

    iniciando projeto

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import "./styled.css";
+
+function Cabecalho() {
+  return (
+    <header>
+      <h1>TESTE</h1>
+      <input type="serch" placeholder="search here for your product" />
+    </header>
+  );
+}
+
+export default Cabecalho;
