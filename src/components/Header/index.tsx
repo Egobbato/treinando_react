4aae2258031e59bd221ebd0b6fed28b5100cf867
Eduardo Gobbato <102391940+Egobbato@users.noreply.github.com>
2022-07-12 21:06:09 -0300

    iniciando Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,10 @@
 import { Header } from "./styled";
 import logo from "../../assets/img/logo.png";
 import lupa from "../../assets/icon/lupa.png";
+import shape from "../../assets/icon/shape.png";
+import boneco from "../../assets/icon/boneco.png";
+import coracao from "../../assets/icon/coracao.png";
+import carrinho from "../../assets/icon/carrinho.png";
 
 function Cabecalho() {
   return (
@@ -10,8 +14,37 @@ function Cabecalho() {
           <img src={logo} alt="skale"></img>
         </div>
         <div className="input">
-          <img src={lupa} alt="lupa"></img>
-          <input type="search" placeholder="search here for your product" />
+          <img className="input-input_img" src={lupa} alt="lupa"></img>
+
+          <input
+            className="input-input"
+            type="search"
+            placeholder="search here for your product"
+          />
+        </div>
+        <div className="icones">
+          <ul>
+            <li>
+              <a href="home">
+                <img className="icones-icones" src={shape} alt="shape"></img>
+              </a>
+            </li>
+            <li>
+              <a href="home">
+                <img className="icones-icones" src={boneco} alt="shape"></img>
+              </a>
+            </li>
+            <li>
+              <a href="home">
+                <img className="icones-icones" src={coracao} alt="shape"></img>
+              </a>
+            </li>
+            <li>
+              <a href="home">
+                <img className="icones-icones" src={carrinho} alt="shape"></img>
+              </a>
+            </li>
+          </ul>
         </div>
       </nav>
     </Header>
--- src/components/Header/styled.ts
+++ src/components/Header/styled.ts
@@ -6,4 +6,34 @@ export const Header = styled.header`
   background-color: #f26522;
   flex-direction: column;
   display: flex;
+
+  nav {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    height: 100%;
+  }
+
+  input {
+    width: 660px;
+    height: 48px;
+    background: #fafafa;
+    border-radius: 16px;
+    font-family: "Courier New", Courier, monospace;
+  }
+
+  li {
+    list-style: none;
+  }
+
+  ul {
+    display: flex;
+    justify-content: space-between;
+    padding: 0px;
+  }
+
+  .icones-icones {
+    width: 70%;
+    justify-content: space-between;
+  }
 `;
