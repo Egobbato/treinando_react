788ee89698e87611d0309c524a484562f2ed46df
Eduardo Gobbato <102391940+Egobbato@users.noreply.github.com>
2022-07-15 09:02:34 -0300

    criação do navBar

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -9,6 +9,36 @@ export function Menu() {
             <p>All Categories</p>
           </a>
         </li>
+        <li>
+          <a href="home">
+            <p>Outdors</p>
+          </a>
+        </li>
+        <li>
+          <a href="home">
+            <p>Indors</p>
+          </a>
+        </li>
+        <li>
+          <a href="home">
+            <p>Water</p>
+          </a>
+        </li>
+        <li>
+          <a href="home">
+            <p>Kids</p>
+          </a>
+        </li>
+        <li>
+          <a href="home">
+            <p>Women</p>
+          </a>
+        </li>
+        <li>
+          <a href="home">
+            <p>Men</p>
+          </a>
+        </li>
       </ul>
     </MenuHamburg>
   );
--- src/components/Menu/styled.ts
+++ src/components/Menu/styled.ts
@@ -1,11 +1,24 @@
 import styled from "styled-components";
 
 export const MenuHamburg = styled.nav`
+  ul {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+  }
+
   li {
     list-style: none;
   }
 
   a {
     text-decoration: none;
+  }
+
+  p {
+    font-family: "Courier New";
+    font-weight: 400;
+    color: #1f1f1f;
+    font-size: 24px;
   }
 `;
