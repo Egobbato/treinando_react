468e3acf84ac3e58b3cab08a0706fe1d088ba51c
Eduardo Gobbato <102391940+Egobbato@users.noreply.github.com>
2022-07-18 09:37:09 -0300

    finalizando Box Inicial

--- src/components/Box_inicial/index.tsx
+++ src/components/Box_inicial/index.tsx
@@ -1,10 +1,12 @@
 import banner_inicial from "../../assets/img/banner_inicial.png";
 import { Background } from "./styled";
+import texto from "../../assets/img/texto.png";
 
 function BoxInicial() {
   return (
     <Background className="banner">
       <article className="banner_inicial">
+        <img className="banner_inicia-_texto" src={texto} alt="texto"></img>
         <img
           className="banner_inicial_foto"
           src={banner_inicial}
--- src/components/Box_inicial/styled.ts
+++ src/components/Box_inicial/styled.ts
@@ -4,11 +4,21 @@ export const Background = styled.section`
   .banner_inicial {
     background: #d9d9d9;
     text-align: center;
+    height: 400px;
   }
 
-  img {
-    width: 1280px;
+  .banner_inicial_foto {
+    padding-top: 58px;
+    width: auto;
     height: 280px;
     border-radius: 16px;
+  }
+
+  .banner_inicia-_texto {
+    border: 5px solid #fafafa;
+    color: #1f1f1f;
+    position: absolute;
+    margin-top: 106px;
+    margin-left: 77px;
   }
 `;
